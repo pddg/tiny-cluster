ed50f85282f0d79d124e3f09b13e7e504a10fdac
Pudding <s.kokuryo@gmail.com>
2020-10-10 16:13:35 +0900

    Format

--- pkg/models/host.go
+++ pkg/models/host.go
@@ -2,26 +2,26 @@ package models
 
 // MachineSpec is a spec of the host.
 type MachineSpec struct {
-    // Core is a number of CPU core.
-    Core int
-    // Memory is an amount of DRAM (MB).
-    Memory int
-    // Disk is an amount of local disk (GB).
-    Disk int
+	// Core is a number of CPU core.
+	Core int
+	// Memory is an amount of DRAM (MB).
+	Memory int
+	// Disk is an amount of local disk (GB).
+	Disk int
 }
 
 // Machine is a information of phisical host
 type Machine struct {
-    // ID is a unique index of this host.
-    ID string
-    // MAC is Media Access Control address of this host
-    MAC string
-    // Name is a friendly name.
-    Name string
-    // IPv4Addr is a IPv4 address of this host.
-    IPv4Addr string
-    // DeployedDate is a UNIX time of the date when this host is deployed.
-    DeployedDate int
-    // Spec indicates the machine spec of the host.
-    Spec MachineSpec
+	// ID is a unique index of this host.
+	ID string
+	// MAC is Media Access Control address of this host
+	MAC string
+	// Name is a friendly name.
+	Name string
+	// IPv4Addr is a IPv4 address of this host.
+	IPv4Addr string
+	// DeployedDate is a UNIX time of the date when this host is deployed.
+	DeployedDate int
+	// Spec indicates the machine spec of the host.
+	Spec MachineSpec
 }
--- pkg/repositories/machine.go
+++ pkg/repositories/machine.go
@@ -1,21 +1,20 @@
 package repositories
 
 import (
-    "context"
+	"context"
 
-    "github.com/pddg/tiny-cluster/pkg/models"
+	"github.com/pddg/tiny-cluster/pkg/models"
 )
 
 // MachineRepository is a repository about Machine.
 type MachineRepository interface {
-    // GetMachines returns all machines.
-    // This returns empty list and no error if no machines were found.
-    GetMachines(ctx context.Context) ([]*models.Machine, error)
-    // RegisterMachine creates a record of the machine.
-    // This returns error when the item has been created.
-    RegisterMachine(ctx context.Context, machine *models.Machine) error
-    // DeleteMachine deletes the record of the machine.
-    // This returns error when the item does not exist.
-    DeleteMachine(ctx context.Context, machine *models.Machine) error
+	// GetMachines returns all machines.
+	// This returns empty list and no error if no machines were found.
+	GetMachines(ctx context.Context) ([]*models.Machine, error)
+	// RegisterMachine creates a record of the machine.
+	// This returns error when the item has been created.
+	RegisterMachine(ctx context.Context, machine *models.Machine) error
+	// DeleteMachine deletes the record of the machine.
+	// This returns error when the item does not exist.
+	DeleteMachine(ctx context.Context, machine *models.Machine) error
 }
-
