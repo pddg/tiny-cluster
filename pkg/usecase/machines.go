99dcfa3238abee0a8ae1ed8feb2dfbb849be7e94
Pudding <s.kokuryo@gmail.com>
2020-10-12 22:33:03 +0900

    Implement MachineQuery to filter the machines

--- pkg/usecase/machines.go
+++ pkg/usecase/machines.go
@@ -1,3 +1,4 @@
+//go:generate mockgen -source=$GOFILE -destination=mock/$GOFILE -package=mock
 package usecase
 
 import (
@@ -10,7 +11,7 @@ import (
 type MachineUsecase interface {
 	GetAllMachines(ctx context.Context) ([]*models.Machine, error)
 	GetMachineByName(ctx context.Context, name string) (*models.Machine, error)
-	GetMachineByQuery(ctx context.Context, query map[string]string) ([]*models.Machine, error)
+	GetMachineByQuery(ctx context.Context, query *MachineQuery) ([]*models.Machine, error)
 }
 
 type machineUseCaseImpl struct {
@@ -21,17 +22,34 @@ func (m *machineUseCaseImpl) GetAllMachines(ctx context.Context) ([]*models.Mach
 	return m.repo.GetMachines(ctx)
 }
 
-func (m *machineUseCaseImpl) GetMachineByQuery(ctx context.Context, query map[string]string) ([]*models.Machine, error) {
+func (m *machineUseCaseImpl) GetMachineByName(ctx context.Context, name string) (*models.Machine, error) {
+	query := &MachineQuery{"name": name}
+	machines, err := m.GetMachineByQuery(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	if len(machines) == 0 {
+		return nil, nil
+	}
+	return machines[0], nil
+}
+
+func (m *machineUseCaseImpl) GetMachineByQuery(ctx context.Context, query *MachineQuery) ([]*models.Machine, error) {
 	machines, err := m.repo.GetMachines(ctx)
 	if err != nil {
 		return nil, err
 	}
 	var matchedMachines []*models.Machine
 	for _, machine := range machines {
-		if mac := query["mac"]; len(mac) > 0 {
-			if machine.MAC == mac {
-				matchedMachines = 
-			}
+		if query.Match(machine) {
+			matchedMachines = append(matchedMachines, machine)
 		}
+	}
+	return matchedMachines, nil
+}
+
+func NewMachineUseCase(repo repositories.MachineRepository) MachineUsecase {
+	return &machineUseCaseImpl{
+		repo: repo,
 	}
 }
---
+++ pkg/usecase/utils.go
@@ -0,0 +1,22 @@
+package usecase
+
+import (
+	"github.com/pddg/tiny-cluster/pkg/models"
+)
+
+type MachineQuery map[string]string
+
+func (q MachineQuery) Match(machine *models.Machine) bool {
+	var match bool
+	for k, v := range q {
+		switch {
+		case k == "mac":
+			match = machine.MAC == v
+		case k == "name":
+			match = machine.Name == v
+		case k == "ipv4":
+			match = machine.IPv4Addr == v
+		}
+	}
+	return match
+}
