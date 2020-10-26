596a3377cb5881ec91b92298b28065791060cbf5
Pudding <s.kokuryo@gmail.com>
2020-10-26 23:39:15 +0900

    add json tag to the model

--- pkg/models/machines.go
+++ pkg/models/machines.go
@@ -3,23 +3,23 @@ package models
 // MachineSpec is a spec of the host.
 type MachineSpec struct {
 	// Core is a number of CPU core.
-	Core int
+	Core int `json:"core"`
 	// Memory is an amount of DRAM (MB).
-	Memory int
+	Memory int `json:"memory"`
 	// Disk is an amount of local disk (GB).
-	Disk int
+	Disk int `json:"disk"`
 }
 
 // Machine is a information of phisical host
 type Machine struct {
 	// MAC is Media Access Control address of this host
-	MAC string
+	MAC string `json:"mac"`
 	// Name is a friendly name.
-	Name string
+	Name string `json:"name"`
 	// IPv4Addr is a IPv4 address of this host.
-	IPv4Addr string
+	IPv4Addr string `json:"ipv4_addr"`
 	// DeployedDate is a UNIX time of the date when this host is deployed.
-	DeployedDate int64
+	DeployedDate int64 `json:"deployed_date"`
 	// Spec indicates the machine spec of the host.
-	Spec MachineSpec
+	Spec MachineSpec `json:"spec"`
 }
